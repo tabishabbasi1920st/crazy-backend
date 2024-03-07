2db8e9bb8b19a3132228820727b611f03285f7dc
tabishabbasidev <tabishabbasidev@gmail.com>
2024-03-07 13:23:34 +0530

    Added to mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const fs = require("fs");
 const https = require("https");
+const { Server } = require("socket.io");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
@@ -20,6 +23,31 @@ const httpsOptions = {
 };
 
 const server = https.createServer(httpsOptions, app);
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+    ],
+    methods: ["GET", "POST"],
+  },
+});
+
+// Connect to mongodb atlas.
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connection successful to DB");
+  } catch (error) {
+    console.error("Database connection failed");
+    process.exit(0);
+  }
+};
+
+connectDb();
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running at port: ${PORT}`);
