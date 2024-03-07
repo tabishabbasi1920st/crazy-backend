3c74baac945f0671cc90680798a3e7e7629812e8
tabishabbasidev <tabishabbasidev@gmail.com>
2024-03-07 13:35:10 +0530

    Fix login api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const LoginOrRegisterModel = require("./models/loginOrRegisterModel");
+const { sign } = require("jsonwebtoken");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
