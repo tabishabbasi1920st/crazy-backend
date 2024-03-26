bc3bf058ec1a65f4bd3ab59800bb2b43a65b42be
tabishabbasi1920st <tabishabbasi1920st@gmail.com>
2024-03-26 16:05:49 +0530

    Updated cors

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const io = new Server(server, {
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:3002",
+      "https://crazychat.netlify.app/",
     ],
     methods: ["GET", "POST"],
   },
